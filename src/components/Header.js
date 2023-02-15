22f4f3aef868bae192f885d37401a99fa8adcf1c
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-02-15 13:13:40 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+// style
+import "../style/header.css";
+
+const Header = () => {
+  return <header>Les reprises ROCK des Mercy</header>;
+};
+
+export default Header;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// style
+import "../style/home.css";
+
+const Home = () => {
+  return (
+    <main className="home">
+      <div className="empty"></div>
+      <div className="title">Playlist</div>
+    </main>
+  );
+};
+
+export default Home;
