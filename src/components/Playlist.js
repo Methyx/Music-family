5095735ceccd4a535727eb0121b72b405f92b1cb
Philippe MERCY <philippemercy@macbook-pro-de-philippe-1.home>
2023-02-16 10:50:29 +0100

    playlist in 3D carousel

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import playlist from "../assets/playlist.json";
 import PlaylistTitle from "../components/PlaylistTitle";
 
@@ -5,21 +7,43 @@ import PlaylistTitle from "../components/PlaylistTitle";
 import "../style/playlist.css";
 
 const Playlist = ({ setTitleToPlay }) => {
+  const [rotate, setRotate] = useState(0);
   return (
     <div className="playlist">
       <h1>On écoute quoi ?</h1>
-      <div className="items">
-        {playlist.map((item, index) => {
-          return (
-            <div className="song">
-              <PlaylistTitle
-                key={index}
-                item={item}
-                setTitleToPlay={setTitleToPlay}
-              />
-            </div>
-          );
-        })}
+      <div className="songs-container">
+        <div
+          className="songs-carousel"
+          style={{
+            transform: `translateZ(-100px) rotateX(${rotate}deg)`,
+          }}
+        >
+          {playlist.map((item, index) => {
+            return (
+              <div className="song">
+                <PlaylistTitle
+                  key={index}
+                  item={item}
+                  setTitleToPlay={setTitleToPlay}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <button
+          onClick={() => {
+            setRotate((r) => r - 60);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setRotate((r) => r + 60);
+          }}
+        >
+          -
+        </button>
       </div>
     </div>
   );
