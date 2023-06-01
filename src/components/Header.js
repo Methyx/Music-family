9382e12f553a5cb59db2863cbe7318f9ea726941
Methyx <methyx95@gmail.com>
2023-06-01 17:26:51 +0200

    second try auto deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@
 import "../style/header.css";
 
 const Header = () => {
-  return <header>Les reprises ROCK des Mercy</header>;
+  return <header>Reprises ROCK des Mercy</header>;
 };
 
 export default Header;
